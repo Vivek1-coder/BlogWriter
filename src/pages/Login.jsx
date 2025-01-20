2bdd11b7a062b729a640292f72a50a33371c1946
Vivek <vy212205@gmail.com>
2025-01-20 12:01:08 +0530

    first commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Login as LoginComponent } from '../components'
+function Login() {
+  return (
+    <div className='py-8'>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab iure neque alias. Modi eveniet consequuntur incidunt consectetur autem ipsa. Eveniet adipisci cupiditate illo fugiat porro, numquam perferendis, consequatur ad quibusdam maxime ut nulla accusantium!</p>
+      <LoginComponent/>
+    </div>
+  )
+}
+
+export default Login
